e908561d27949ff082682817a56826be18443dcc
JohnKelvin <johnkelvin254@live.com>
2024-07-21 23:22:38 +0300

    add js file

---
+++ quiz.js
@@ -0,0 +1,21 @@
+function checkAnswer() {
+  const correctAnswer = "4";
+  const userAnswer = document.querySelector(
+    'input[name="quiz"]:checked'
+  )?.value;
+
+  if (userAnswer) {
+    if (userAnswer === correctAnswer) {
+      feedbackElement.textContent = "Correct! Well done.";
+      feedbackElement.style.color = "green";
+    } else {
+      feedbackElement.textContent = "That's incorrect. Try again!";
+      feedbackElement.style.color = "red";
+    }
+  } else {
+    feedbackElement.textContent = "Please select an answer.";
+    feedbackElement.style.color = "orange";
+  }
+
+  submitButton.addEventListener("click", checkAnswer);
+}
